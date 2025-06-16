4a768b4e9465ba4ccfd98d11dac43da6c25275e5
Lembahmanah <lembahku@gmail.com>
2025-06-16 20:02:57 +0700

    Initial Commit for this project

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // LESSON 1: PRINT OUT
+        // ketik sout and then press tab to system.out.print
+        // println was for biar ada ke bawah, kalo gak di ln bakalan lurus teksnya
+        //System.out.println("Aku suka makan");
+        //System.out.println("Pecel dan ayam goreng");
+
+        // Lesson 2 : Variables
+        // ❎ variable = A reusable container for a value.
+        //                           A variable behaves as if it was the value it contains.
+
+        // 🟥 Primitive = simple value stored directly in memory (stack). its like giving someone a dollar cash
+        // 🟦 Reference = memory address (stack) that points to the (heap). its like having an agreement that you give him a dollar, and then give it later
+
+        // 🟥 Primitive vs 🟦 Reference
+        //    ---------                      ---------
+        //    int                           string
+        //    double  (like float)          array
+        //    char    (a single character)  object
+        //    boolean
+
+        //int age = 21;
+        //int year = 2025;
+        //int quantity = 1;
+
+        //double gpa = 3.5;
+        //double price = 19.88;
+        //double temperature = -12.2;
+
+        //char grade = 'A';
+        //char symbol = '!';
+        //char curency = '$';
+
+        //boolean isStudent = true;
+        //boolean forSale = true;
+        //boolean isOnline = false;
+
+        //String name = "Lembah";
+        //String domisili = "Malang";
+        //String email = "lembahku@gmail.com";
+        //String car = "brio";
+        //String warna = "silver";
+
+        //System.out.println("Hello!, namaku " + name + ". Aku dari " + domisili);
+        //System.out.println("Aku menaiki mobil " + car + " tahun " + year + " seharga " + curency + " " + price);
+
+        //if (isOnline){                              // Bacanya: if isonline = true:.... disini { dan } nya itu kayak : di python, lihat if dan eslenya. setiap command di akhiri; idk why
+            //System.out.println("Your friend is online");
+        //}
+        //else{
+            //System.out.println("Your friend is not online");
+        //}
+
+        
+
+    }
+}
